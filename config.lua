dd9c398859fcaff220d885f43001d333e78043a9
The Hunter V <44531252+TheHunterV@users.noreply.github.com>
2023-06-30 22:57:41 -0400

    Add files via upload
    uploaded Config file , Server.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config ={
+    JobCenter = vector3(931.89, -2267.38, 30.84),
+    ReAdd = 60, -- Seconds after the job has been finished until it is shown again
+    Job = {
+        ['jobRequired'] = true, -- IF true: only players with the specified job can work, false everyone can work
+        ['jobName'] = 'trucker',
+    }
+    Jobs = {
+        --{title = 'title', payment = reward, vehicles = {'truck', 'trailer'}, start = {vector3(x, y, z), heading}, trailer = {vector3(x, y, z), heading}, arrive = vector3(x, y, z)}
+        {}
+    }
+}
